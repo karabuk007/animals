9b0b2c33d2c335e3c3d39b1889616d96e444723c
karabuk007 <igortrebunskiy@yandex.ru>
2023-03-20 14:12:04 +0300

    delete dogs.html

--- main_page/views.py
+++ main_page/views.py
@@ -6,8 +6,8 @@ def index(request):
     return HttpResponse('<h1>Ура, я что-то написал!</h1>')
 def for_cats(request):
     posts = Cats.objects.all()
-    return render(request, 'cats.html', {'posts': posts, 'page_title': 'Котики'})
+    return render(request, 'animals.html', {'posts': posts, 'page_title': 'Котики'})
 
 def for_dogs(request):
     posts = Dogs.objects.all()
-    return render(request, 'dogs.html', {'posts': posts, 'page_title': 'Пёсики'})
+    return render(request, 'animals.html', {'posts': posts, 'page_title': 'Пёсики'})
